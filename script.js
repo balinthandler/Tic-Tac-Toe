e42dc1ce5721fe98ec1f4ad79caaca87fbd573da
balinthandler <handlerbalint@gmail.com>
2020-04-19 19:02:51 +0200

    winning mechanism created

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const gameBoard = (() =>{
     const boardArray = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
     const boardContainer = document.querySelector('#boardContainer');
+    let playerOneMarks = [];
+    let playerTwoMarks = [];
+    let player1Mark = 'X';
+    let player2Mark = 'O';
     let lastMark = '';
     let player1 = {};
     let player2 = {};
+    const winningCases = [
+        ['A1', 'A2', 'A3'],
+        ['B1', 'B2', 'B3'],
+        ['C1', 'C2', 'C3'],
+        ['A1', 'B1', 'C1'],
+        ['A2', 'B2', 'C2'],
+        ['A3', 'B3', 'C3'],
+        ['A1', 'B2', 'C3'],
+        ['A3', 'B2', 'C1']
+    ]
     const playerInit = (name, marker) => {
         return{
             name,
-            marker,
-            points: 0
+            marker
         }
     }
 
@@ -21,18 +34,46 @@ const gameBoard = (() =>{
             newbox.id = element;
             boardContainer.appendChild(newbox);
         });
+        let winning = () => {
+            const vsInfo = document.querySelector('#vsInfo');
+            vsInfo.textContent = ''
+        }
         let box = document.querySelectorAll('.box');
         box.forEach(e => {e.addEventListener('click', function() {
-            if (lastMark == '' || lastMark == 'O') {
-                this.innerHTML = 'X';
-                lastMark = 'X';
-            } else if (lastMark == 'X') {
-                this.innerHTML = 'O'
-                lastMark = 'O';
+            if ((lastMark == '' || lastMark == player2Mark) && this.innerHTML == '') {
+                this.innerHTML = player1Mark;
+                lastMark = player1Mark;
+                playerOneMarks.push(this.id);
+            } else if (lastMark == player1Mark && this.innerHTML == '') {
+                this.innerHTML = player2Mark;
+                lastMark = player2Mark;
+                playerTwoMarks.push(this.id);
             }
 
-        })});
+            let evaluate = function evaluate(array) {
+                let win = false;
+                winningCases.forEach(w => {
+                    let found = 0;
+                    w.forEach(e => {
+                        array.forEach(v => {
+                            if (v == e) {
+                                found +=1;
+                                if (found == 3) {
+                                    console.log('WIN')}
+                            }
+                        })
+                    })
 
+                })
+                
+
+            }
+            evaluate(playerOneMarks)
+
+
+
+        })});
+        
     }
 
 
@@ -77,8 +118,9 @@ const gameBoard = (() =>{
     return{
         playerInit,
         newGameInput,
-        createBoard
-        
+        createBoard,
+        winningCases,
+        playerOneMarks,
     }
     
     
